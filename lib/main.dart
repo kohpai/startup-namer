545933f16db5b9445aca3373f66c458b08e97ba9
Kohpai <whcwhc78@gmail.com>
2022-09-17 19:39:11 +0200

    :lipstick: show favorite icon

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class RandomWords extends StatefulWidget {
 // by default, the name of the State class is prefixed with an underscore
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -48,10 +49,17 @@ class _RandomWordsState extends State<RandomWords> {
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
+          final alreadySaved = _saved.contains(_suggestions[index]);
+
           return ListTile(
             title: Text(
               _suggestions[index].asPascalCase,
               style: _biggerFont,
+            ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel: alreadySaved ? 'Removed from saved' : 'Saved',
             ),
           );
         });
